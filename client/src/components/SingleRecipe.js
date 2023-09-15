0a6d4755b7b8c0c047c3cc50e858ccea8e464bd4
nadjaob <nadja.oblaender@gmx.de>
2023-09-15 21:30:12 +0200

    fixed search function and passed properties to single recipe page

--- client/src/components/SingleRecipe.js
+++ client/src/components/SingleRecipe.js
@@ -1,3 +1,45 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+// COMPONENTS
+import Spinner from './Spinner'
+
+// BOOTSTRAP
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 export default function SingleRecipe() {
-  return <h1>SingleRecipe</h1>
+
+  const [ recipe, setRecipe ] = useState()
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getRecipeData = async () => {
+      try {
+        const { data } = await axios.get(`/api/recipes/${id}`)
+        setRecipe(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getRecipeData()
+  }, [])
+
+
+  return (
+    <>
+      { recipe ?
+        <Container>
+          <Row>
+            <Col><h1>{recipe.title}</h1></Col>
+          </Row>
+        </Container>
+        :
+        <Spinner />
+      }
+    </>
+    
+  )
 }
