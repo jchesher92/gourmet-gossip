54a8e61e3cb39244f6b8ddc106975f0d4ffa5d20
Alex Parker <parkeralexjm@gmail.com>
2023-09-16 12:17:29 +0100

    Added user info to profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,24 +1,28 @@
 import axios from 'axios'
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { UserContext } from '../App'
 import { getToken } from '../utility/auth'
 import { useNavigate } from 'react-router'
 
 export default function Profile() {
   const { user, setUser } = useContext(UserContext)
+  const [profile, setProfile] = useState({})
   const token = getToken()
   const redirect = useNavigate()
 
   useEffect(() => {
     async function getUserProfile() {
       try {
-        if (user) {
+        if (token) {
           const { data } = await axios.get('/api/profile', {
             headers: {
               'Authorization': token,
             },
           })
+          setProfile(data)
           console.log(data)
+        } else {
+          redirect('/login')
         }
       } catch (error) {
         console.log(error)
@@ -26,5 +30,20 @@ export default function Profile() {
     }
     getUserProfile()
   }, [])
-  return <h1>Profile</h1>
+
+  return (
+    <>
+      <section className='container profile-container'>
+        <h1>Profile</h1>
+        {profile &&
+          <div className='profile-info'>
+            <h1>Profile Name: {profile.username}</h1>
+            <h1>Email: {profile.email}</h1>
+            <h1>Recipes added: {profile.recipesAdded}</h1>
+            <h1>Favorites: {profile.favourites}</h1>
+          </div>
+        }
+      </section>
+    </>
+  )
 }
