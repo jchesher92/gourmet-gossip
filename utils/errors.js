69746f5fd49f670f3a79db738cba1d5681f8293b
jchesher92 <jchesher92@gmail.com>
2023-09-18 15:19:54 +0100

    error.js handler added

---
+++ utils/errors.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose'
+
+// Custom classes
+// All custom classes are going to extend the native ErrorConstructor in order to throw error into a catch with a message
+class CustomError extends Error {
+  constructor(message, details) {
+    super(message)
+    this.details = details
+  }
+}
+
+export class UnprocessableEntity extends CustomError {
+  constructor(message, details) {
+    super(message, details)
+    this.name = 'UnprocessableEntity'
+    this.status = 422
+  }
+}
+
+export class NotBound extends CustomError {
+  constructor(message, details) {
+    super(message, details)
+    this.name = 'NotFound'
+    this.status = 404
+  }
+}
+
+export class Unauthorised extends CustomError {
+  constructor(message, details) {
+    super(message, details)
+    this.name = 'Unauthorised'
+    this.status = 401
+  }
+}
+
+export const checkId = (id) => {
+  if (!mongoose.isValidObjectId(id)){
+    throw new UnprocessableEntity('Invalid Object Id', { 
+      ObjectId: {
+        name: 'Failed ObjectId Validation',
+        message: 'Invalid Object Id'
+      } 
+    })
+  }
+}
+
+export const sendErrors = (error, res) => {
+
+  const { message, name, status, details } = error
+
+  status = status || 422
+
+  console.log('-----------------------')
+  console.log('Errors  ')
+  console.log('-----------------------')
+  console.log('Name:', status, name)
+  console.log('Message:', message)
+  console.log('details:', details)
+  console.log('-----------------------')
+
+  console.log(status || 422)
+
+  return res.status(status || 422).json({ errors: details })
+}
