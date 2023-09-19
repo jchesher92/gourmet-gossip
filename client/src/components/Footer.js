fb7536340aa0839e9e71ef3a805f86d4af92d3f9
nadjaob <nadja.oblaender@gmx.de>
2023-09-19 09:35:03 +0200

    added icons to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom'
 
+// ICON
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 // BOOTSTRAP
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -12,9 +16,18 @@ export default function Footer() {
         <Row>
           <Col className='footer'>
             <h6>Project by</h6>
-            <p><Link target="_blank" to="https://github.com/parkeralexjm">Alex Parker</Link></p>
-            <p><Link target="_blank" to="https://github.com/jchesher92">James Chesher</Link></p>
-            <p><Link target="_blank" to="https://github.com/nadjaob">Nadja Oblaender</Link></p>
+            <p>
+              <Link target="_blank" to="https://github.com/parkeralexjm">
+                <FontAwesomeIcon icon={faGithub} style={{ color: '#fff' }} /> Alex Parker</Link>
+            </p>
+            <p>
+              <Link target="_blank" to="https://github.com/jchesher92">
+                <FontAwesomeIcon icon={faGithub} style={{ color: '#fff' }} /> James Chesher</Link>
+            </p>
+            <p>
+              <Link target="_blank" to="https://github.com/nadjaob">
+                <FontAwesomeIcon icon={faGithub} style={{ color: '#fff' }} /> Nadja Oblaender</Link>
+            </p>
           </Col>
         </Row>
       </Container>
